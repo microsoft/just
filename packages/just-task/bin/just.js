672bdcc6e8d59ab6f387e401f1a1dc673abf1a5f
Ken <kchau@microsoft.com>
2018-12-07 09:15:16 -0800

    adds resilience and better error messaging in the CLI script

--- packages/just-task/bin/just.js
+++ packages/just-task/bin/just.js
@@ -1,8 +1,17 @@
 #!/usr/bin/env node
 
+let localCmd = null;
+
 try {
-  const localCmd = require.resolve('just-task/lib/cli.js', { paths: [process.cwd()] });
-  require(localCmd);
+  localCmd = require.resolve('just-task/lib/cli.js', { paths: [process.cwd()] });
 } catch (e) {
   console.error('Please install a local copy of just-task.');
+  process.exit(1);
+}
+
+try {
+  require(localCmd);
+} catch (e) {
+  console.log('Just encountered an error', e);
+  process.exit(1);
 }
