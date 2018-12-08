8586b8c241d6236ae0f6f8630699c8403f401ce1
Ken <kchau@microsoft.com>
2018-12-07 20:55:08 -0800

    streamlines API so the logger is not from the this context

--- scripts/watch.js
+++ scripts/watch.js
@@ -1,6 +1,6 @@
 const { spawn } = require('child_process');
 
-['', 'typescript', 'webpack'].forEach(pkg => {
+['', 'preset'].forEach(pkg => {
   const name = ['just', 'task', ...(pkg ? [pkg] : [])];
 
   const cp = spawn('tsc', ['-p', `packages/${name.join('-')}`, '-w', '--preserveWatchOutput'], { stdio: 'pipe' });
