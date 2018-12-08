76f884bb0e2dae6bcf14a5d45df8c992cd1c477f
Ken <kchau@microsoft.com>
2018-12-07 20:58:29 -0800

    updates example-lib to work with new api

--- packages/example-lib/just-task.js
+++ packages/example-lib/just-task.js
@@ -1,6 +1,10 @@
-const { task, series, parallel } = require('just-task');
+const { task, parallel } = require('just-task');
+const { tscTask } = require('just-task-preset');
 
 require('just-task-preset');
+
+task('typescript', tscTask({}));
+task('typescript:watch', tscTask({ watch: true }));
 
 task('build', parallel('typescript'));
 task('watch', parallel('typescript:watch'));
