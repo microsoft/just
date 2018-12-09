ada9b9ba3b9630117efce05ec650b6d67f8456dc
Ken <kchau@microsoft.com>
2018-12-08 23:34:36 -0800

    adding a tslint task

--- packages/just-task-preset/src/index.ts
+++ packages/just-task-preset/src/index.ts
@@ -3,3 +3,4 @@ export * from './cleanTask';
 export * from './copyTask';
 export * from './sassTask';
 export * from './jestTask';
+export * from './tslintTask';
--- packages/just-task-preset/src/tslintTask.ts
+++ packages/just-task-preset/src/tslintTask.ts
@@ -1,8 +1,22 @@
-import { logger } from 'just-task';
+import { resolve, logger, resolveCwd } from 'just-task';
+import { exec } from './exec';
+import path from 'path';
 
-export function jestTask() {
-  return function jest(done: (err?: Error) => void) {
-    logger.info(`Running Jest`);
-    done();
+export interface ITsLintTaskOptions {
+  config?: string;
+}
+
+export function tslintTask(options: ITsLintTaskOptions = {}) {
+  const projectFile = resolveCwd('./tsconfig.json');
+
+  return function tslint() {
+    logger.info(`Running tslint`);
+    const tslintCmd = resolve('tslint/lib/tslintCli.js');
+
+    const args = ['--project', projectFile, '-t', 'stylish', '-r', path.dirname(resolve('tslint-microsoft-contrib') || '')];
+
+    const cmd = [process.execPath, tslintCmd, ...args].join(' ');
+    logger.info(cmd);
+    return exec(cmd, { stdout: process.stdout, stderr: process.stderr });
   };
 }
