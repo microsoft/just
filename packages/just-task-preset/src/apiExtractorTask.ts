c7b1b83c53546d308aa8b0ba7976aace2ffb7788
Ken <kchau@microsoft.com>
2018-12-15 15:18:39 -0800

    details about api-extractor tasks

--- packages/just-task-preset/src/apiExtractorTask.ts
+++ packages/just-task-preset/src/apiExtractorTask.ts
@@ -2,9 +2,34 @@ import { logger } from 'just-task';
 import { Extractor } from '@microsoft/api-extractor';
 import { IExtractorConfig, IExtractorOptions } from '@microsoft/api-extractor/dist/index-internal';
 
-export function apiExtractorTask(extractorConfig: IExtractorConfig, extractorOptions: IExtractorOptions) {
-  // This interface represents the API Extractor config file contents
-  const config: IExtractorConfig = {
+export function apiExtractorVerifyTask(config: IExtractorConfig, options: IExtractorOptions) {
+  return function apiExtractorVerify() {
+    if (!apiExtractorWrapper(config, options)) {
+      throw 'The public API file is out of date. Please run "npm run update-api" and commit the updated API file.';
+    }
+  };
+}
+
+export function apiExtractorUpdateTask(config: IExtractorConfig, options: IExtractorOptions) {
+  return function apiExtractorUpdate() {
+    if (!apiExtractorWrapper(config, options)) {
+      logger.warn(`- Update API: API file is out of date, updating...`);
+      apiExtractorWrapper(config, { ...options, localBuild: true });
+      logger.info(`- Update API: successfully updated API file, verifying the updates...`);
+
+      if (!apiExtractorWrapper(config, options)) {
+        throw Error(`- Update API: failed to update API file.`);
+      } else {
+        logger.info(`- Update API: successully verified API file. ` + `Please commit API file as part of your changes.`);
+      }
+    } else {
+      logger.info(`- Update API: API file is already up to date, no update needed.`);
+    }
+  };
+}
+
+function mergeConfig(extractorConfig: IExtractorConfig): IExtractorConfig {
+  return {
     compiler: {
       configType: 'tsconfig',
       rootFolder: './'
@@ -20,15 +45,11 @@ export function apiExtractorTask(extractorConfig: IExtractorConfig, extractorOpt
     },
     ...extractorConfig
   };
+}
 
-  return function apiExtractor(done: (err?: Error) => void) {
-    logger.info(`Extracting Public API surface from '${config.project.entryPointSourceFile}'`);
-    const extractor = new Extractor(config, extractorOptions);
-    const success = extractor.processProject();
-    if (!success) {
-      throw 'The public API file is out of date. Please run "npm run update-api" and commit the updated API file.';
-    }
-
-    done();
-  };
+function apiExtractorWrapper(extractorConfig: IExtractorConfig, extractorOptions: IExtractorOptions) {
+  const config = mergeConfig(extractorConfig);
+  logger.info(`Extracting Public API surface from '${config.project.entryPointSourceFile}'`);
+  const extractor = new Extractor(config, extractorOptions);
+  return extractor.processProject();
 }
