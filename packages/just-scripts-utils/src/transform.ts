1b92681aed2c339d9fddc78103c6824ec99998e1
Elizabeth Craig <elcraig@microsoft.com>
2019-02-06 21:08:24 -0800

    Tests and error handling for transform

--- packages/just-scripts-utils/src/transform.ts
+++ packages/just-scripts-utils/src/transform.ts
@@ -2,30 +2,126 @@ import glob from 'glob';
 import path from 'path';
 import fse from 'fs-extra';
 import handlebars from 'handlebars';
+import { logger } from './logger';
 
-export function transform(srcPath: string, destPath: string, data?: any) {
-  const templateFiles = [...new Set([...glob.sync('**/*', { cwd: srcPath }), ...glob.sync('**/.*', { cwd: srcPath })])];
+export interface ITransformResult {
+  /** Whether a fatal error occurred */
+  error?: boolean;
+  /** Number of files/folders processed successfully (not counting warnings) */
+  processed?: number;
+  /** Number of files/folders for which processing was unsuccessful */
+  warnings?: number;
+}
 
-  if (!fse.existsSync(destPath)) {
-    fse.mkdirpSync(destPath);
+/**
+ * Apply a template to a project directory. If the directory doesn't exist, it will be created.
+ * If the directory exists, contents will be merged with the files/folders from the template.
+ *
+ * @param templateDir Path to the directory containing the template
+ * @param projectDir Path to the destination project to create/update
+ * @param templateData If the template contains any handlebars files, this will be passed to them
+ * when compiling
+ * @returns Object indicating whether the transform was successful
+ */
+export function transform(
+  templateDir: string,
+  projectDir: string,
+  templateData?: any
+): ITransformResult {
+  let templateFiles: string[];
+  try {
+    templateFiles = [...new Set(glob.sync('**/*', { cwd: templateDir, dot: true }))];
+  } catch (ex) {
+    logger.error(`Error finding template files under ${templateDir}: ${ex}`);
+    return { error: true };
   }
 
+  if (!fse.existsSync(projectDir)) {
+    try {
+      fse.mkdirpSync(projectDir);
+    } catch (ex) {
+      logger.error(`Couldn't create directory ${projectDir}: ${ex}`);
+      return { error: true };
+    }
+  }
+
+  let processed = 0;
+  let warnings = 0;
   templateFiles
     .filter(name => name.indexOf('.DS_Store') < 0)
     .forEach(templateFile => {
-      const inputFile = path.join(srcPath, templateFile);
-      const outputFile = path.join(destPath, templateFile);
-
-      const stat = fse.statSync(inputFile);
-
-      if (path.extname(templateFile) === '.hbs' && stat.isFile()) {
-        const template = handlebars.compile(fse.readFileSync(inputFile).toString());
-        const results = template(data);
-        fse.writeFileSync(outputFile.replace(/\.hbs$/, ''), results);
-      } else if (stat.isFile()) {
-        fse.copySync(inputFile, outputFile, { overwrite: true });
+      if (_processFileFromTemplate(templateFile, templateDir, projectDir, templateData)) {
+        ++processed;
       } else {
-        fse.mkdirpSync(outputFile);
+        ++warnings;
       }
     });
+
+  return { processed, warnings };
+}
+
+/**
+ * Exported for testing only. Processes a file or folder from a template.
+ * @param templateFile Path to a file/folder that's part of a template, relative to template root
+ * @param templateDir Path to the folder the template is being copied from
+ * @param projectDir Path to the folder the template is being copied to
+ * @param templateData If the file is a handlebars template, this will be passed to it when compiling
+ * @returns true if no errors occur
+ */
+export function _processFileFromTemplate(
+  templateFile: string,
+  templateDir: string,
+  projectDir: string,
+  templateData?: any
+): boolean {
+  const inputFilePath = path.join(templateDir, templateFile);
+  const outputFilePath = path.join(projectDir, templateFile);
+
+  if (!fse.existsSync(inputFilePath)) {
+    logger.warn(`File ${inputFilePath} does not exist. Skipping.`);
+    return false;
+  }
+
+  try {
+    const inputStat = fse.statSync(inputFilePath);
+    const inputIsDir = inputStat.isDirectory();
+    if (fse.existsSync(outputFilePath)) {
+      const outputIsDir = fse.statSync(outputFilePath).isDirectory();
+      if (inputIsDir !== outputIsDir) {
+        logger.warn(
+          `Template ${templateFile} is a ${inputIsDir ? 'directory' : 'file'} ` +
+            `in source but a ${outputIsDir ? 'directory' : 'file'} in destination. Skipping.`
+        );
+        return false;
+      }
+    }
+
+    if (inputIsDir) {
+      fse.mkdirpSync(outputFilePath);
+    } else {
+      if (path.extname(templateFile) === '.hbs') {
+        _writeHbsFile(inputFilePath, outputFilePath, templateData);
+      } else {
+        fse.copySync(inputFilePath, outputFilePath, { overwrite: true });
+      }
+    }
+  } catch (ex) {
+    logger.warn(`Error processing template ${templateFile} (skipping): ${ex}`);
+    return false;
+  }
+  return true;
+}
+
+/**
+ * Exported for testing only. Process a handlebars template.
+ * @param inputFile Full path to the handlebars template
+ * @param outputFile Full path to the destination file
+ * @param templateData Data passed to the template when compiling
+ */
+export function _writeHbsFile(inputFile: string, outputFile: string, templateData?: any) {
+  // Intentionally allowing exceptions to propagate and be handled by caller
+  const fileContents = fse.readFileSync(inputFile).toString();
+  const template = handlebars.compile(fileContents);
+  const results = template(templateData);
+  fse.writeFileSync(outputFile.replace(/\.hbs$/, ''), results);
 }
