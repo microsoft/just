ff3112e23e8cc11e61a67b2fc2d42ab52e2e4576
Ken <kchau@microsoft.com>
2019-04-10 15:37:14 -0700

    make the just-stack.json write out to a specific installed version of stack

--- packages/just-scripts/src/stack/lockfile.ts
+++ packages/just-scripts/src/stack/lockfile.ts
@@ -1,11 +1,21 @@
 import { getAvailableStacks } from './getAvailableStacks';
 import path from 'path';
 import fs from 'fs';
+import { readPackageJson } from 'just-scripts-utils';
 
 export function writeLockFile(rootPath: string) {
   const stacks = getAvailableStacks(rootPath);
+
+  const resolvedStacks: { [key: string]: string } = {};
+  Object.keys(stacks).forEach(stack => {
+    const packageJson = readPackageJson(path.join(stack, 'package.json'));
+    if (packageJson) {
+      resolvedStacks[stack] = packageJson.version;
+    }
+  });
+
   const lockFile = path.join(rootPath, 'scripts', 'just-stacks.json');
-  fs.writeFileSync(lockFile, JSON.stringify({ stacks }, null, 2));
+  fs.writeFileSync(lockFile, JSON.stringify({ stacks: resolvedStacks }, null, 2));
 }
 
 export function readLockFile(rootPath: string) {
