1329c9aa954c58367b06b4f0e877004002048305
Kenneth Chau <kenotron@users.noreply.github.com>
2019-04-25 11:10:47 -0700

    get rid of dep0097 warnings (#81)
    * get rid of dep0097 warnings
    
    * change file

--- packages/just-task/src/cli.ts
+++ packages/just-task/src/cli.ts
@@ -2,6 +2,22 @@ import { undertaker } from './undertaker';
 import { JustTaskRegistry } from './JustTaskRegistry';
 import yargs from 'yargs';
 
+const originalEmitWarning = process.emitWarning;
+process.emitWarning = function emitWarning(warning: string | Error, name?: string, ctor?: Function) {
+  if (name === 'DEP0097') {
+    // Undertaker uses a deprecated approach that causes NodeJS 10 to print
+    // this warning to stderr:
+    //
+    // "Using a domain property in MakeCallback is deprecated. Use the  async_context
+    // variant of MakeCallback or the AsyncResource class instead."
+
+    // Suppress the warning!
+    return;
+  }
+
+  return originalEmitWarning(warning, name, ctor);
+};
+
 yargs
   .option({ config: { describe: 'path to a just-task.js file (includes the file name)' } })
   .usage('$0 <cmd> [options]')
