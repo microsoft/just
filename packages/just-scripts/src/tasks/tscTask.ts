0279043cfdbb34a839cd1b7b9f889682f41301a0
Ken <kchau@microsoft.com>
2019-06-12 11:30:52 -0700

    supports tsc build project ref mode

--- packages/just-scripts/src/tasks/tscTask.ts
+++ packages/just-scripts/src/tasks/tscTask.ts
@@ -1,5 +1,5 @@
 import ts from 'typescript';
-import { resolve, logger, resolveCwd, TaskFunction } from 'just-task';
+import { resolve, logger, resolveCwd, TaskFunction, option } from 'just-task';
 import { exec, encodeArgs, spawn } from 'just-scripts-utils';
 import fs from 'fs';
 
@@ -15,9 +15,9 @@ export function tscTask(options: TscTaskOptions): TaskFunction {
 
   return function tsc() {
     // Read from options argument, if not there try the tsConfigFile found in root, if not then skip and use no config
-    options.project = (options && options.project) || tsConfigFile || undefined;
+    options = { ...options, ...getProjectOrBuildOptions(options) };
 
-    if (options.project && fs.existsSync(options.project as string)) {
+    if (isValidProject(options)) {
       logger.info(`Running ${tscCmd} with ${options.project}`);
 
       const args = Object.keys(options).reduce(
@@ -43,17 +43,16 @@ export function tscTask(options: TscTaskOptions): TaskFunction {
 }
 
 export function tscWatchTask(options: TscTaskOptions): TaskFunction {
-  const tsConfigFile = resolveCwd('./tsconfig.json');
   const tscCmd = resolve('typescript/lib/tsc.js');
 
   if (!tscCmd) {
     throw new Error('cannot find tsc');
   }
 
-  return function tscWatch() {
-    options.project = options.project || tsConfigFile || undefined;
+  options = { ...options, ...getProjectOrBuildOptions(options) };
 
-    if (options.project && fs.existsSync(options.project as string)) {
+  return function tscWatch() {
+    if (isValidProject(options)) {
       logger.info(`Running ${tscCmd} with ${options.project} in watch mode`);
 
       const args = Object.keys(options).reduce(
@@ -76,4 +75,23 @@ export function tscWatchTask(options: TscTaskOptions): TaskFunction {
       Promise.resolve();
     }
   };
+}
+
+function getProjectOrBuildOptions(options: TscTaskOptions) {
+  const tsConfigFile = resolveCwd('./tsconfig.json');
+  const result: { [option: string]: string | boolean } = {};
+
+  if (options.project) {
+    result.project = options && typeof options.project === 'string' ? options.project : tsConfigFile || true;
+  } else if (options.build) {
+    result.build = options && typeof options.build === 'string' ? options.build : tsConfigFile || true;
+  } else if (tsConfigFile) {
+    result.project = tsConfigFile;
+  }
+
+  return result;
+}
+
+function isValidProject(options: TscTaskOptions) {
+  return (options.project && fs.existsSync(options.project as string)) || (options.build && fs.existsSync(options.build as string));
 }
