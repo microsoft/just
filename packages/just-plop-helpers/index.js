bd9712ef158c843fbf3c20d01d066f1a9f123e55
Ken <kchau@microsoft.com>
2019-08-05 11:35:28 -0700

    adding a defaultinclude option to make sure actiontypes are shared

--- packages/just-plop-helpers/index.js
+++ packages/just-plop-helpers/index.js
@@ -2,4 +2,5 @@ const renameAction = require('./actions/renameAction');
 
 module.exports = plop => {
   plop.setActionType('rename', renameAction);
+  plop.setDefaultInclude({ actionTypes: true });
 };
