bcb1f45b53956000e9fe03e6eff84c1f27d9375a
OneDrive Build <odbuild@microsoft.com>
2019-09-27 09:24:53 -0700

    adding loaderoptions for tsoverlay

--- packages/just-scripts/src/webpack/overlays/tsOverlay.ts
+++ packages/just-scripts/src/webpack/overlays/tsOverlay.ts
@@ -1,24 +1,39 @@
+import ts from 'typescript';
 import { tryRequire } from '../../tryRequire';
 
 const ForkTsCheckerPlugin = tryRequire('fork-ts-checker-webpack-plugin');
 
-export const tsOverlay = () => ({
-  resolve: {
-    extensions: ['.wasm', '.mjs', '.js', '.ts', '.tsx', '.json']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: {
-          loader: 'ts-loader',
-          options: {
-            transpileOnly: true
-          }
-        },
-        exclude: /node_modules/
-      }
-    ]
-  },
-  plugins: [...(ForkTsCheckerPlugin ? [new ForkTsCheckerPlugin()] : [])]
-});
+export interface LoaderOptions {
+  configFile: string;
+  transpileOnly: boolean;
+  onlyCompileBundledFiles: boolean;
+  colors: boolean;
+  compilerOptions: ts.CompilerOptions;
+  happyPackMode: boolean;
+  getCustomTransformers: string | ((program: ts.Program) => ts.CustomTransformers | undefined);
+  experimentalWatchApi: boolean;
+  allowTsInNodeModules: boolean;
+  experimentalFileCaching: boolean;
+  projectReferences: boolean;
+}
+
+export const tsOverlay = (options: Partial<LoaderOptions> = { transpileOnly: true }) => {
+  return {
+    resolve: {
+      extensions: ['.wasm', '.mjs', '.js', '.ts', '.tsx', '.json']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: {
+            loader: 'ts-loader',
+            options
+          },
+          exclude: /node_modules/
+        }
+      ]
+    },
+    plugins: [...(ForkTsCheckerPlugin ? [new ForkTsCheckerPlugin()] : [])]
+  };
+};
