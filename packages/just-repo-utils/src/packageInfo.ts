d180ac84619e8e81c963db4e87fa628b24c73cc0
Jason Morse <35609178+JasonVMo@users.noreply.github.com>
2020-05-20 18:58:47 -0700

    Add cwd support for repo utils (#382)
    * add ability to query packages or roots from a different working directory
    
    * Change files
    
    * fix incorrect caching when cwd is set

--- packages/just-repo-utils/src/packageInfo.ts
+++ packages/just-repo-utils/src/packageInfo.ts
@@ -1,17 +1,28 @@
 import { PackageInfoOptions, PackageInfo } from './interfaces/packageInfoTypes';
 import { getRepoInfo } from './repoInfo';
 
+function validateOptions(options?: PackageInfoOptions): PackageInfoOptions {
+  options = options || {};
+  // if a current working directory is being specified the primary use case is querying dependencies or packages of a different
+  // repository.  In this case, any information retrieved shouldn't be stored in our cache as it may or may not be relevant
+  if (options.cwd) {
+    options.strategy = 'no-cache';
+  }
+  return options;
+}
+
 /**
  * retrieves information about the packages in the repository
  * @param strategy - cache strategy to use for loading, defaults to normal
  */
 export function getPackageInfo(options?: PackageInfoOptions): PackageInfo {
+  options = validateOptions(options);
   const { retrievePackageInfo, cachePackageInfo } = require('./internal/packageInfoCache');
   const { infoFromEntries, buildPackageInfoFromGlobs, buildPackageInfoFromRushProjects } = require('./internal/packageInfoHelpers');
-  const { strategy = 'normal' } = options || {};
+  const { strategy = 'normal' } = options;
   let repoPackageInfo = strategy === 'normal' && retrievePackageInfo();
   if (!repoPackageInfo) {
-    const repo = getRepoInfo();
+    const repo = getRepoInfo(options);
     if (repo.getLernaJson) {
       repoPackageInfo = buildPackageInfoFromGlobs(repo.rootPath, repo.getLernaJson().packages);
     } else if (repo.getRushJson) {
