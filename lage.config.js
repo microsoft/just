63a2748a89d91634aa92cf64c445990d6df4625f
Chris Trevino <darthtrevino@gmail.com>
2020-10-06 07:25:26 -0700

    Add cacheoptions to lage config

--- lage.config.js
+++ lage.config.js
@@ -1,10 +1,14 @@
 module.exports = {
   pipeline: {
     build: ['^build'],
-    test: ['build']
+    test: ['build'],
   },
-
+  cacheOptions: {
+    outputGlob: ['dist/**/*', 'lib/**/*', '!node_modules'],
+    internalCacheFolder: '.cache',
+    logFolder: '.cache',
+  },
   // These are relative to the git root, and affects the hash of the cache
   // Any of these file changes will invalidate cache
-  environmentGlob: ['*.js', '*.json', '*.yml']
+  environmentGlob: ['*.js', '*.json', '*.yml'],
 };
