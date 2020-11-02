6d279b510b12a3fae8da50032dafd8a71b2195bc
Christian Gonzalez <1581488+christiango@users.noreply.github.com>
2020-11-02 09:57:49 -0800

    Ignore types

--- packages/just-scripts-utils/src/prettyPrintMarkdown.ts
+++ packages/just-scripts-utils/src/prettyPrintMarkdown.ts
@@ -11,7 +11,8 @@ export function prettyPrintMarkdown(content: string): string {
     return '';
   }
   marked.setOptions({
-    renderer: new TerminalRenderer()
+    // The typings for marked-terminal have not been updated in a few years, so it results in compiler errors with the newer versions of marked
+    renderer: new TerminalRenderer() as any
   });
   return marked(content);
 }
