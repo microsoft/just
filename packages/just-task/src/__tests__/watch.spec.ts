5b83fed3a5c8d5930eb0d5c6b474756898ed3ce3
Ken <kchau@microsoft.com>
2021-03-19 16:38:21 -0700

    adding a happy path test

---
+++ packages/just-task/src/__tests__/watch.spec.ts
@@ -0,0 +1,30 @@
+import { watch } from '../watch';
+import * as fs from 'fs';
+import * as path from 'path';
+import * as os from 'os';
+
+describe('watch', () => {
+  it('can take a synchronous taskFunction', done => {
+    const tmpDir = path.join(os.tmpdir(), fs.mkdtempSync('watch-sync'));
+    const changeFile = path.join(tmpDir, 'change.txt');
+
+    fs.mkdirSync(tmpDir, { recursive: true });
+    fs.writeFileSync(changeFile, 'to be changed');
+
+    const callback = () => {
+      try {
+        expect(true).toBeTruthy();
+        watcher.close();
+        done();
+      } catch (error) {
+        watcher.close();
+        done(error);
+      }
+    };
+    const watcher = watch([path.join(changeFile)], callback);
+
+    watcher.on('ready', () => {
+      fs.writeFileSync(changeFile, 'new content');
+    });
+  });
+});
