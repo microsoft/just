d683f2c65cd7b0e4f47047e4d414c945f8315f18
Kenneth Chau <34725+kenotron@users.noreply.github.com>
2021-03-22 12:46:21 -0700

    Adds esbuildTask() (#527)
    * adds a watch()
    
    * adds a watch()
    
    * Change files
    
    * glob watch
    
    * adding a happy path test
    
    * cleanup tmp
    
    * rmdir
    
    * okay node 12 sucks
    
    * adding esbuild to just-scripts
    
    * Change files
    
    * merge

---
+++ packages/just-scripts/src/tasks/esbuildTask.ts
@@ -0,0 +1,24 @@
+import type { BuildOptions } from 'esbuild';
+import { resolve, TaskFunction } from 'just-task';
+
+export type EsbuildBuildOptions = BuildOptions;
+export interface EsbuildTransformOptions {
+  esbuildOptions: BuildOptions;
+  include: string[] | string;
+}
+
+/**
+ * creates a esbuild task function, checking for esbuild's presence; can be used for bundling or building
+ */
+export function esbuildTask(options: EsbuildBuildOptions = {}): TaskFunction {
+  const esbuildModuleResolution = resolve('esbuild');
+
+  if (!esbuildModuleResolution) {
+    throw new Error('cannot find esbuild, please add it to your devDependencies');
+  }
+
+  return async function esbuild() {
+    const esbuildModule = require(esbuildModuleResolution) as typeof import('esbuild');
+    return esbuildModule.build(options);
+  };
+}
