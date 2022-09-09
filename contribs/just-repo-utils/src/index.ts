8c63dc5f76e8bc93eacc9df81750e5ef7119a665
Elizabeth Craig <elcraig@microsoft.com>
2022-09-08 20:26:20 -0700

    Remove unused contribs/just-repo-utils

--- contribs/just-repo-utils/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { PackageJson, RushJson, LernaJson } from './interfaces/configTypes';
-export * from './cacheUtils';
-export * from './gitUtils';
-export * from './normalizeToUnixPath';
-export * from './packageInfo';
-export * from './readConfigs';
-export * from './repoInfo';
--- contribs/just-repo-utils/src/normalizeToUnixPath.ts
+++
@@ -1,9 +0,0 @@
-import * as path from 'path';
-
-/**
- * take a path, call path.normalize, then make sure it uses forward slashes
- * @param base - path to put into forward slashed form
- */
-export function normalizeToUnixPath(base: string): string {
-  return path.normalize(base).replace(/\\/g, '/');
-}
