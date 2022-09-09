522a7412f81ef07c9004a26f6c6259aa5e26fb59
Elizabeth Craig <elcraig@microsoft.com>
2022-09-09 16:29:47 -0700

    Delete unused just-plop-helpers package

--- packages/just-plop-helpers/index.js
+++
@@ -1,8 +0,0 @@
-const renameAction = require('./actions/renameAction');
-const addDependencyAction = require('./actions/addDependencyAction');
-
-module.exports = plop => {
-  plop.setActionType('rename', renameAction);
-  plop.setActionType('addDependency', addDependencyAction);
-  plop.setDefaultInclude({ actionTypes: true });
-};
