8d487af7c6de3abe574921f3c408e0eeb922867e
Elizabeth Craig <elcraig@microsoft.com>
2023-03-08 22:39:51 -0800

    Remove unused functions and related deps from just-scripts-utils (#696)

--- packages/just-scripts-utils/src/index.ts
+++ packages/just-scripts-utils/src/index.ts
@@ -1,10 +1,7 @@
 export * from './exec';
-export * from './findMonoRepoRootPath';
 export * from './interfaces/PackageJson';
 export * from './interfaces/RushJson';
 export * from './logger';
 export { mergePackageJson } from './mergePackageJson';
 export * from './paths';
-export * from './prettyPrintMarkdown';
 export * from './readPackageJson';
-export { readRushJson, rushAddPackage, rushUpdate } from './rush';
