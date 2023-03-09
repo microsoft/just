013f64313b703351a4cb8c9bb86cbcac4be7e83e
Elizabeth Craig <elcraig@microsoft.com>
2023-03-08 22:19:26 -0800

    remove more

--- packages/just-scripts-utils/src/index.ts
+++ packages/just-scripts-utils/src/index.ts
@@ -1,9 +1,7 @@
 export * from './exec';
-export * from './findMonoRepoRootPath';
 export * from './interfaces/PackageJson';
 export * from './interfaces/RushJson';
 export * from './logger';
 export { mergePackageJson } from './mergePackageJson';
 export * from './paths';
 export * from './readPackageJson';
-export { readRushJson, rushAddPackage, rushUpdate } from './rush';
