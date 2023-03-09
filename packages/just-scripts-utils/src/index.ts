8f6c80c99063302f5555beb7459b69360b598c3f
Elizabeth Craig <elcraig@microsoft.com>
2023-03-08 21:56:47 -0800

    Remove unused prettyPrintMarkdown function and related deps

--- packages/just-scripts-utils/src/index.ts
+++ packages/just-scripts-utils/src/index.ts
@@ -5,6 +5,5 @@ export * from './interfaces/RushJson';
 export * from './logger';
 export { mergePackageJson } from './mergePackageJson';
 export * from './paths';
-export * from './prettyPrintMarkdown';
 export * from './readPackageJson';
 export { readRushJson, rushAddPackage, rushUpdate } from './rush';
