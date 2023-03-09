8bc1b1bfe6db217824ae979540ee2c775d60be29
Elizabeth Craig <elcraig@microsoft.com>
2023-03-08 23:58:34 -0800

    Update to TS 4.3 and Jest 29 (#698)

--- scripts/jest.config.js
+++ scripts/jest.config.js
@@ -1,16 +1,22 @@
+// @ts-check
 const path = require('path');
 
-/** Jest config for packages within the just monorepo */
+/**
+ * Jest config for packages within the just monorepo
+ * @type {import('@jest/types').Config.InitialOptions}
+ */
 module.exports = {
   roots: ['<rootDir>/src'],
-  preset: 'ts-jest',
   testEnvironment: 'node',
   testMatch: ['**/?(*.)+(spec|test).[jt]s'],
   verbose: true,
-  globals: {
-    'ts-jest': {
-      tsconfig: path.resolve(process.cwd(), 'tsconfig.json'),
-      packageJson: path.resolve(process.cwd(), 'package.json'),
-    },
+  transform: {
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: path.join(process.cwd(), 'tsconfig.json'),
+        packageJson: path.join(process.cwd(), 'package.json'),
+      },
+    ],
   },
 };
